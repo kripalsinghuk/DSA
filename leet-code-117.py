9e1c14504a6b5b66c5e44a50563d9682fbc17f62
kripalsinghuk <kirpalsinghuk@gmail.com>
2026-03-17 23:36:45 +0530

    did two point type qustion 1

---
+++ leet-code-117.py
@@ -0,0 +1,15 @@
+#167. Two Sum II - Input Array Is Sorted
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(numbers)-1
+        while i<j:
+            sum=numbers[i]+numbers[j]
+            if sum == target:
+                return [i+1,j+1]
+            elif sum < target:
+                i=i+1
+            elif sum > target:
+                j=j-1
+        return [0,0]
