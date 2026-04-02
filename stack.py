86904f5999a2ea70f5c2cdb8d195f2171ed3be19
kripalsinghuk <kirpalsinghuk@gmail.com>
2026-04-02 09:12:16 +0530

    added is_empty, size, and peek methods in stack.

--- stack.py
+++ stack.py
@@ -1,22 +1,38 @@
 class Stack:
+    #push,pop,is_empty,peek,size
 
     def __init__(self):
         self.myList = []
-
     def push(self,item):
         self.myList.append(item)   
     def pop(self):
-        self.myList.pop(-1)     
+        self.myList.pop(-1)  
+    def peek(self):
+        length = len(self.myList)
+        if length == 0:
+            raise ValueError("Empty list!")
+        else:
+            return self.myList[length-1]
+    def is_empty(self):
+        if len(self.myList) > 0:
+            return False
+        else:
+            return True
+    def size(self):
+        return len(self.myList)    
     def print(self):
         for l in self.myList:
             print(l)    
 
 
 stack = Stack()
+print(stack.is_empty())
 stack.push(2)
 stack.push(4)
 stack.push(7)
-stack.print()
-stack.pop()
-stack.print()
+# stack.print()
+# stack.pop()
+stack.peek()
+# stack.print()
+print(stack.size())
 
