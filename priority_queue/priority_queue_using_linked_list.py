199cee9dc87cf1b1cfb0ae7253dabbf4098e5cc8
Kripal Singh <kripalsingh@ishivax.com>
2026-04-09 16:29:16 +0530

    implementation of priority queue using linked list is done and tested.

--- priority_queue/priority_queue_using_linked_list.py
+++ priority_queue/priority_queue_using_linked_list.py
@@ -10,28 +10,13 @@ class PriorityQueue:
         self.head = None
         self.count = 0
     def size(self):
-        return count
+        return self.count
     def is_empty(self):
         if self.count>0:
             return False
         else:
             return True
-    def insert_at_start(self,val,pr):
-        temp = self.head
-        newNode = Node(None,val,temp,pr)
-        self.head = newNode
-        temp.prev = newNode
-    def insert_at_last(self,val,pr):
-        if self.head is None:
-            newNode = Node(Node,val,None,pr)
-            self.head = newNode
-        else:
-            temp = self.head
-            while temp.next is not None:
-                temp = temp.next
-            else:
-                newNode = Node(temp,val,None,pr)
-                temp.next = newNode
+    
     def push(self,val,pr):
         self.count+=1
         if self.head is None:
@@ -40,25 +25,38 @@ class PriorityQueue:
         else:
             temp = self.head
             while temp.next is not None:
-                if pr < temp.pr:
+                if pr <= temp.pr:
                     newNode = Node(temp.prev,val,temp,pr)
-                    temp.prev.next = newNode
-                    temp.prev = newNode
+                    if temp.prev is not None:
+                        temp.prev.next = newNode
+                        temp.prev = newNode
+                    else:
+                        self.head = newNode
+                        temp.prev = newNode
                     break
                 temp = temp.next
             else:
                 if pr <= temp.pr:
+                    newNode = Node(temp.prev,val,temp,pr)
                     if temp.prev is not None:
-                        newNode = Node(temp.prev,val,temp,pr)
                         temp.prev.next = newNode
                         temp.prev = newNode
                     else:
-                        newNode = Node(temp.prev,val,temp,pr)
                         self.head = newNode
-
                 else:
                     newNode = Node(temp,val,None,pr)
                     temp.next = newNode
+    def pop(self):
+        if self.head is None:
+            raise Exception("empty queue")
+        else:
+            temp = self.head
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+            self.count-=1
+            return temp.val
+
     def printQueue(self):
         temp = self.head
         if temp is None:
@@ -71,14 +69,19 @@ class PriorityQueue:
 
 q = PriorityQueue()
 q.push(10,4)
-q.push(33,0)
+q.push(33,5)
+q.push(89,0)
 q.push(909,3)
 q.push(89,2)
 q.push(89,1)
 q.push(3,92)
 q.push(3,92)
 q.push(3,93)
+q.push(99,0)
+q.push(100,1)
 q.printQueue()
-
-
+print(q.pop())
+q.printQueue()
+print(q.size())
+print(q.is_empty())
 
